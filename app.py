c76f32e40f275246ac1bf02309f22a2b91136eea
JMICylinders <jmicylindersohs@gmail.com>
2025-05-05 15:48:37 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,31 +4,26 @@ import os
 
 app = Flask(__name__)
 
-# SMS API configuration
+# SMS API Configuration
 API_URL = "http://bulksmsbd.net/api/smsapi"
 API_KEY = "t6qElhvPR1nyZ8prIC0C"
 SENDER_ID = "8809617625650"
+RECIPIENTS = "+8801725692402,+8801325095925"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     status = None
     if request.method == "POST":
-        location = request.form.get("location")
-        number = "+8801725692402,+8801324436107,+8801332549219,+8801777742732"
-        message = (
-                    f"⚠️ FIRE ALERT at {location}!\n\n"
-                    "Evacuate the area IMMEDIATELY.\n"
-                    "Follow the nearest emergency exit signs and report to the assembly point.\n"
-                    "Do NOT use elevators.\n"
-                    "Stay calm and assist others if possible.\n\n"
-                    "🚨 Emergency response team must take action NOW.\n\n"
-                    "- Safety Team")
-
+        if "predefined" in request.form:
+            location = request.form.get("location")
+            message = f"""⚠️ FIRE ALERT at {location}!\n\nEvacuate the area IMMEDIATELY.\nFollow the nearest emergency exit signs and report to the assembly point.\nDo NOT use elevators.\nStay calm and assist others if possible.\n\n🚨 Emergency Response Team: Take Immediate Action."""
+        elif "custom" in request.form:
+            message = request.form.get("custom_message")
 
         payload = {
             "api_key": API_KEY,
             "senderid": SENDER_ID,
-            "number": number,
+            "number": RECIPIENTS,
             "message": message
         }
 
